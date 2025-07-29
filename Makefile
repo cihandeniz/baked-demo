ff921e838fd504cb11eea1333bfa8bf6224395aa
cihandeniz <cihan@gcbrains.com>
2025-07-29 20:57:25 +0300

    add demo web app

--- Makefile
+++ Makefile
@@ -1,8 +1,23 @@
-.PHONY: build test run
+.PHONY: install build test run
 
+install:
+	@ \
+	cd src/Demo.Web ; npm i ; cd ../.. ; \
+	cd src/Demo.Web ; npm ci ; cd ../..
 build:
 	@ dotnet build
 test:
 	@ dotnet test --logger quackers
 run:
-	@ dotnet run --project src/Demo.App
+	@ \
+	echo "(1) backend" ; \
+	echo "(2) frontend" ; \
+	echo "" ; \
+	echo "Please select 1-2: " ; \
+	read app ; \
+	if test $$app -eq "1" ; then \
+		dotnet run --project src/Demo.App ; \
+	fi ; \
+	if test $$app -eq "2" ; then \
+		cd src/Demo.Web ; npm run -s dev ; cd ../.. ; \
+	fi
---
+++ src/Demo.Web/nuxt.config.ts
@@ -0,0 +1,46 @@
+import Aura from "@primeuix/themes/aura";
+import { definePreset } from "@primeuix/themes";
+import app from "./.baked/app.json" assert { type: "json" };
+
+const Mouseless = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: "{red.50}",
+      100: "{red.100}",
+      200: "{red.200}",
+      300: "{red.300}",
+      400: "{red.400}",
+      500: "{red.500}",
+      600: "{red.600}",
+      700: "{red.700}",
+      800: "{red.800}",
+      900: "{red.900}",
+      950: "{red.950}"
+    }
+  }
+});
+
+export default defineNuxtConfig({
+  baked: {
+    app: app,
+    components: {
+      Page: {
+        title: "Demo"
+      }
+    },
+    composables: {
+      useDataFetcher: {
+        baseURL: process.env.API_BASE_URL,
+        retry: true
+      }
+    },
+    primevue: {
+      theme: Mouseless
+    }
+  },
+  compatibilityDate: "2025-04-02",
+  components: { dirs: ["~/components"] },
+  css: [ "~/assets/styles.scss" ],
+  modules: [ "@nuxt/eslint", "baked-recipe-admin" ],
+  router: { options: { strict: true } }
+});
