1d5cbe85b8d13a1144e9e2989c0c98b73e70b9f5
cihandeniz <cihan@gcbrains.com>
2025-07-29 22:38:31 +0300

    add room for ui customizations

--- src/Demo.App/Program.cs
+++ src/Demo.App/Program.cs
@@ -1,7 +1,11 @@
-Bake.New
+﻿Bake.New
     .Service(
         authentications: [],
         business: c => c.DomainAssemblies(typeof(Worklog).Assembly),
-        database: c => c.Sqlite("Demo.App.db")
+        database: c => c.Sqlite("Demo.App.db"),
+        configure: app =>
+        {
+            app.Features.Add(new UiCustomizations());
+        }
     )
     .Run();
---
+++ src/Demo.App/UiCustomizations.cs
@@ -0,0 +1,22 @@
+﻿using Baked.Architecture;
+
+public class UiCustomizations : IFeature
+{
+    public void Configure(LayerConfigurator configurator)
+    {
+        configurator.ConfigureAppDescriptor(app =>
+        {
+
+        });
+
+        configurator.ConfigureLayoutDescriptors(layouts =>
+        {
+
+        });
+
+        configurator.ConfigurePageDescriptors(pages =>
+        {
+
+        });
+    }
+}
